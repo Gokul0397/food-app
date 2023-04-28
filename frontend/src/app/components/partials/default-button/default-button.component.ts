efa5ff8b64b9d6a5c0792c0efc3dc41f241b7319
gokulakrishnan01 <gokul@iprimofocus.com>
2023-04-28 17:34:25 +0530

    default button component added

--- frontend/src/app/components/partials/default-button/default-button.component.ts
+++ frontend/src/app/components/partials/default-button/default-button.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-default-button',
+  selector: 'default-button',
   templateUrl: './default-button.component.html',
   styleUrls: ['./default-button.component.css']
 })
 export class DefaultButtonComponent {
-
+  @Input()
+  type : 'submit'|'button' = 'submit'
+  @Input()
+  text:string = "submit"
+  @Input()
+  bgcolor = "#e72929"
+  @Input()
+  color = "white"
+  @Input()
+  fontSizeRem = 1.3;
+  @Input()
+  widthRem = 12;
+  @Output()
+  onClick = new EventEmitter();
 }
